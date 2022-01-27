70bf8c979a426108ef241d4d479107eca11daaf5
Syarguk <90717093+Syarguk@users.noreply.github.com>
2022-01-27 12:13:54 +0300

    Update index.js files
    Add  decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let resStr = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        let subStr = expr.slice(i, i + 10);
+        let curStr = '';
+        if(subStr[0] == '*') {
+            resStr += ' '; 
+            continue;
+        }
+        for (let j = 0; j < subStr.length; j += 2) {
+            if(subStr[j] == '0') continue;
+            if('10' == subStr.slice(j, j + 2)) {
+                curStr += '.';
+            } else {
+                curStr += '-';
+            }
+        }
+        resStr += MORSE_TABLE[curStr];
+    }
+    return resStr;
 }
 
 module.exports = {
